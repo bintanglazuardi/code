a3f997279c895b582d6749f458dbaab84602ebd6
bintanglazuardi <bintanglazuardi@mail.ugm.ac.id>
2020-04-15 16:41:49 +0700

    Updates

---
+++ precache-manifest.3c5a0eb686a1821d441b996112252101.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af387c5766987b91e670f17a00696f8b",
+    "url": "/code/index.html"
+  },
+  {
+    "revision": "f1e74958156f617c1d78",
+    "url": "/code/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "72872b62cd9bc860c186",
+    "url": "/code/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1e74958156f617c1d78",
+    "url": "/code/static/js/2.40633d33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/code/static/js/2.40633d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72872b62cd9bc860c186",
+    "url": "/code/static/js/main.b8cbbe5c.chunk.js"
+  },
+  {
+    "revision": "73166e2c6353ee58deb3",
+    "url": "/code/static/js/runtime-main.94689f85.js"
+  }
+]);
